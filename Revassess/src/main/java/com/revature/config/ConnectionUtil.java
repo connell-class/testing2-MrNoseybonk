67a97b662088b867d760be3f05de95c0485dcb29
Steven Wilson <speedy0097@yahoo.com>
2020-07-01 10:41:39 -0400

    Adding answers for Tier3.

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -16,15 +16,15 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:oracle:thin:@revassesstest.c0ippggfggzg.us-east-2.rds.amazonaws.com:1521:ORCL";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "admin";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "Passw0rd";
 	// name of the created stored procedure in tier 3
-	public static final String TIER_3_PROCEDURE_NAME = "";
+	public static final String TIER_3_PROCEDURE_NAME = "user_study_sets";
 	// name of the created sequence in tier 3
-	public static final String TIER_3_SEQUENCE_NAME = "";
+	public static final String TIER_3_SEQUENCE_NAME = "test_sequence";
 
 	// implement this method to connect to the db and return the connection object
 	public Connection connect(){
